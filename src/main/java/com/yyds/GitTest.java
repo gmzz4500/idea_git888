9876101b2d570ff00e236400b1824dbfc20bf5a3
yuyd <gmzz4500@163.com>
2022-06-02 11:44:38 +0800

    新建文件

---
+++ src/main/java/com/yyds/GitTest.java
@@ -0,0 +1,13 @@
+package com.yyds;
+
+/**
+ * @ClassName: GitTest
+ * @Author: yyd
+ * @Date: 2022/6/2
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("新建文件");
+    }
+}
