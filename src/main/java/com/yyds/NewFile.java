bffa349554802de8abe1c1e86e1929884292a665
yuyd <gmzz4500@163.com>
2022-06-02 13:57:29 +0800

    新建文件

---
+++ src/main/java/com/yyds/NewFile.java
@@ -0,0 +1,13 @@
+package com.yyds;
+
+/**
+ * @ClassName: NewFile
+ * @Author: yyd
+ * @Date: 2022/6/2
+ * @Description:
+ */
+public class NewFile {
+    public static void main(String[] args) {
+        System.out.println("新建文件");
+    }
+}
